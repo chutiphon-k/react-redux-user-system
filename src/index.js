10aa8a06a92d80527a2d2356962fd5e446ec5e98
Chutiphon.k <chutiphon.k@ku.th>
2016-12-21 13:58:34 +0700

    :wrench: Config hot loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
+import React from 'react'
 import { render } from 'react-dom'
-import routes from 'routes'
+import { AppContainer } from 'react-hot-loader'
+import Root from 'Root'
+
+const rootEl = document.getElementById('root')
 
 render(
-  routes(),
-  document.getElementById('root')
+	<AppContainer>
+		<Root />
+	</AppContainer>,
+	rootEl
 )
+
+if (module.hot) {
+  module.hot.accept('Root', () => {
+    const NextRootApp = require('Root').default
+    
+    render(
+      <AppContainer>
+         <NextRootApp />
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
